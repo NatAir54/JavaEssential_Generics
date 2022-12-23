9f9e0d20c3f74b8f74821a4a053b8e05b432aa0e
Nataliia Koval <ruba.fish1500@gmail.com>
2022-12-23 09:55:45 +0200

    sixth commit

--- src/task_5/MyClass.java
+++ src/task_5/MyClass.java
@@ -1,18 +1,32 @@
 package task_5;
 
-public class MyClass <T> {
+import java.util.Arrays;
 
-    public static <T> T factoryMethod(T obj) {
-        return obj;
+public class MyClass <T> {
+    public T field;
+
+    public MyClass(T field) {
+        this.field = field;
+    }
+
+    @Override
+    public String toString() {
+        return "MyClass {" +
+                "field: " + field +
+                '}';
+    }
+
+    public static <T> MyClass<T> factoryMethod(T obj) {
+        return new MyClass<>(obj);
     }
 }
 
 class MainApp {
     public static void main(String[] args) {
-
         System.out.println(MyClass.factoryMethod("my first factory method"));
         System.out.println(MyClass.factoryMethod(0.57));
         System.out.println(MyClass.factoryMethod(88989999900L));
+
     }
 
 }
